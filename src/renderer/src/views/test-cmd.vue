3da89af591d66e382cd9f3a566ce62a4b7974698
bojun1995 <bojun1995@foxmail.com>
2025-04-04 20:29:53 +0800

    init[node-pty]

--- src/renderer/src/views/test-cmd.vue
+++ src/renderer/src/views/test-cmd.vue
@@ -5,55 +5,26 @@
 </template>
 
 <script setup lang="ts">
-import { electron } from 'process';
+import * as pty from 'node-pty'
 
-// import { ipcMain } from 'electron'
-// import { spawn } from 'child_process'
+function onBtnClk(): void {
+  const shell = window.electron.process.platform === 'win32' ? 'powershell.exe' : 'bash'
+  const ptyProcess = pty.spawn(shell, [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 30,
+    cwd: process.env.HOME,
+    env: process.env
+  })
 
-// 根据平台选择 shell
-// const isWindows = process.platform === 'win32'
-// let currentProcess = null
-// let cwd = process.cwd() // 跟踪当前工作目录
+  ptyProcess.onData((data) => {
+    process.stdout.write(data)
+  })
 
-function onBtnClk() {
-  const isWindows = window.electron.process.platform === 'win32'
-  // ipcMain.on('terminal-input', (event, input) => {
-  //   console.log('terminal-input', input)
-  // })
-  debugger
-  try {
-    window.api.ping()
-    debugger
-  } catch (error) {
-    console.error(error)
-  }
+  ptyProcess.write('ls\r')
+  ptyProcess.resize(100, 40)
+  ptyProcess.write('ls\r')
 }
-
-// ipcMain.on('terminal-input', (event, input) => {
-//   if (!currentProcess) {
-//     debugger
-//     // currentProcess = spawn(isWindows ? 'cmd.exe' : 'bash', [], {
-//     //   cwd: cwd,
-//     //   shell: true
-//     // });
-
-//     // // 捕获输出并转发到渲染进程
-//     // currentProcess.stdout.on('data', (data) => {
-//     //   mainWindow.webContents.send('terminal-output', data);
-//     // });
-
-//     // currentProcess.stderr.on('data', (data) => {
-//     //   mainWindow.webContents.send('terminal-output', data);
-//     // });
-
-//     // currentProcess.on('exit', () => {
-//     //   currentProcess = null;
-//     //   mainWindow.webContents.send('terminal-output', '\r\n$ ');
-//     // });
-//   }
-
-//   // currentProcess.stdin.write(input);
-// })
 </script>
 
 <style lang="scss" rel="stylesheet/scss" type="text/scss" scoped>
